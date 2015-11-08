d2e906eb20016b1f25d84b9e8a0adc1b708e7461
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-08 19:51:06 +0100

    C cancelled

---
+++ round/330/C/C.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+/* Mighty Cohadar 
+
+Problem C. Warrior and Archer
+----------
+
+Unfortunately, we are too tell, that there was a mistake in a model solution for problem C. 
+We will remove the problem from the round and the round will be UNRATED.  
+We sincerely apologize for this happening.
+
+*/
+public class C {
+
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
+
