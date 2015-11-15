fd8c1f359f68f4cc775dfaf9b796859011c08319
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-11-16 00:46:21 +0100

    memoization < dp

--- round/331/D/D.java
+++ round/331/D/D.java
@@ -4,6 +4,11 @@ import java.io.*;
 /* Mighty Cohadar */
 public class D {
 
+	static final int LL = 0;
+	static final int RR = 1;
+	static final int LR = 2;
+	static final int RL = 3;
+
 	final int h;
 	final float pl;
 	final float pr;
@@ -11,67 +16,85 @@ public class D {
 	final float ql = 0.5f;
 	final float qr = 0.5f;
 	final float[][][] mem;
+	int count;
 
 	public D(int h, float p, int[] X) {
 		this.h = h;
 		this.pl = p;
 		this.pr = 1.0f - p;
 		this.X = X;
-		this.mem = new float[X.length][X.length][4];
+		this.mem = new float[12][X.length][X.length];
 	}
 
 	float ll(int l, int r, int dl, int dr) {
+		int i = index(dl, dr); // 0123
+		if (mem[i][l][r] != 0) {
+			return mem[i][l][r];
+		}
+		count++;
+		float ret;
 		if (l == r) {
-			return dl;
-		} 		
-		int ndl = Math.min(X[l + 1] - X[l], h);
-		return dl + solve(l + 1, r, ndl, dr); 
+			ret = dl;
+		} else {
+			int ndl = Math.min(X[l + 1] - X[l], h);
+			ret = dl + solve(l + 1, r, ndl, dr);
+		}
+		mem[i][l][r] = ret;
+		return ret;
 	}
 
 	float rr(int l, int r, int dl, int dr) {
+		int i = 4 + index(dl, dr); // 4567
+		if (mem[i][l][r] != 0) {
+			return mem[i][l][r];
+		}
+		count++;
+		float ret;
 		if (l == r) {
-			return dr;
-		} 
-		int ndr = Math.min(X[r] - X[r - 1], h);
-		return dr + solve(l, r - 1, dl, ndr);
+			ret = dr;
+		} else {
+			int ndr = Math.min(X[r] - X[r - 1], h);
+			ret = dr + solve(l, r - 1, dl, ndr);
+		}
+		mem[i][l][r] = ret;
+		return ret;
 	}
 
-	float lr(int l, int r, int dl, int dr) {
+	float lr(int l, int r, int dr) {
+		int i = (dr < h) ? 8 : 9; // 89
+		if (mem[i][l][r] != 0) {
+			return mem[i][l][r];
+		}
+		count++;
+		float ret;
 		if (l == r) {
-			return dr;
-		} 		
-		if (X[l + 1] - X[l] >= h) {
+			ret = dr;
+		} else if (X[l + 1] - X[l] >= h) {
 			int ndl = Math.min(X[l + 1] - (X[l] + h), h);
-			return h + solve(l + 1, r, ndl, dr);
+			ret = h + solve(l + 1, r, ndl, dr);
+		} else {
+			ret = X[l + 1] - X[l] + lr(l + 1, r, dr);
 		}
-		return X[l + 1] - X[l] + lr(l + 1, r, 0, dr);
+		mem[i][l][r] = ret;
+		return ret;
 	}
 
-	float rl(int l, int r, int dl, int dr) {
+	float rl(int l, int r, int dl) {
+		int i = (dl < h) ? 10 : 11; // AB
+		if (mem[i][l][r] != 0) {
+			return mem[i][l][r];
+		}
+		count++;
+		float ret;
 		if (l == r) {
-			return dl;
-		} 		
-		if (X[r] - X[r - 1] >= h) {
+			ret = dl;
+		} else if (X[r] - X[r - 1] >= h) {
 			int ndr = Math.min(h, X[r] - h - X[r - 1]);
-			return h + solve(l, r - 1, dl, ndr);
+			ret = h + solve(l, r - 1, dl, ndr);
+		} else {
+			ret = X[r] - X[r - 1] + rl(l, r - 1, dl);
 		}
-		return X[r] - X[r - 1] + rl(l, r - 1, dl, 0);
-	}	
-
-	public float solve(int l, int r, int dl, int dr) {
-		if (l == r) {
-			return pl * dl + pr * dr;
-		}
-		int i = index(dl, dr);
-		if (mem[l][r][i] != 0) {
-			return mem[l][r][i];
-		}
-		float ret = 0.0f;
-		ret += ql * pl * ll(l, r, dl, dr);
-		ret += qr * pr * rr(l, r, dl, dr);
-		ret += ql * pr * lr(l, r, dl, dr);
-		ret += qr * pl * rl(l, r, dl, dr);
-		mem[l][r][i] = ret;
+		mem[i][l][r] = ret;
 		return ret;
 	}
 
@@ -89,7 +112,19 @@ public class D {
 				return 3;
 			}
 		}
-	}	
+	}
+
+	public float solve(int l, int r, int dl, int dr) {
+		if (l == r) {
+			return pl * dl + pr * dr;
+		}
+		float ret = 0.0f;
+		ret += ql * pl * ll(l, r, dl, dr);
+		ret += qr * pr * rr(l, r, dl, dr);
+		ret += ql * pr * lr(l, r, dr);
+		ret += qr * pl * rl(l, r, dl);
+		return ret;
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -100,6 +135,7 @@ public class D {
 		Arrays.sort(X);
 		D o = new D(h, p, X);
 		System.out.println(o.solve(0, n - 1, h, h));
+		System.err.println(o.count);
 	}
 
 	static int[] scanArray(Scanner scanner, int n) {
