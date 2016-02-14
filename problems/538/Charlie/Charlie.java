78fb06c45d21fccb15f9ecda151396a2c637586e
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-02-14 20:28:40 +0100

    fixup

--- problems/538/Charlie/Charlie.java
+++ problems/538/Charlie/Charlie.java
@@ -4,24 +4,6 @@ import java.io.*;
 /* Mighty Cohadar */
 public class Charlie {
 
-	public static int solve(int max, int[] D, int[] H, int m) {
-		for (int i = 1; i < m; i++) {
-			int hl = H[i - 1];
-			int hr = H[i];
-			int l = D[i - 1];
-			int r = D[i];
-			if (l == r && hl != hr) {
-				return -1;
-			}
-			if (Math.abs(hr - hl) > (r - l)) {
-				return -1;
-			}
-			int h = ((r - l) + hr + hl) / 2;
-			max = Math.max(max, h);
-		}
-		return max;
-	}
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
@@ -29,14 +11,21 @@ public class Charlie {
 		int[] D = new int[m];
 		int[] H = new int[m];
 		for (int i = 0; i < m; i++) {
-			D[i] = scanner.nextInt();
+			D[i] = scanner.nextInt() - 1;
 			H[i] = scanner.nextInt();
 		}
-		int max = H[0];
+		int max = 0;
 		max = Math.max(max, H[0] + D[0]);
 		max = Math.max(max, H[m - 1] + n - 1 - D[m - 1]);
-		int s = solve(max, D, H, m);
-		System.out.println((s == -1) ? "IMPOSSIBLE" : s);
+		for (int i = 1; i < m; i++) {
+			if (Math.abs(H[i] - H[i - 1]) > D[i] - D[i - 1]) {
+				System.out.println("IMPOSSIBLE");
+				return;
+			}
+			int h2 = (D[i] - D[i - 1]) + H[i - 1] + H[i];
+			max = Math.max(max, h2 / 2);
+		}
+		System.out.println(max);
 	}
 
 }
