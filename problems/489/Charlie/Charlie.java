adad2b8054787e536956bc08565a761e7ff4b9bc
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-02-23 10:43:12 +0100

    fail fast

---
+++ problems/489/Charlie/Charlie.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+/* Mighty Cohadar */
+public class Charlie {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int m = scanner.nextInt();
+		assert 1 <= m && m <= 100 : "out of range, m: " + m;
+		int s = scanner.nextInt();
+		assert 0 <= s && s <= 900 : "out of range, s: " + s;
+		
+	}
+
+}
+
