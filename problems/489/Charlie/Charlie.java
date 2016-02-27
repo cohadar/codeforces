d822278e1e95ad87d7f1826e462f3e6ee6ba59ca
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-02-27 17:49:07 +0100

    489C

--- problems/489/Charlie/Charlie.java
+++ problems/489/Charlie/Charlie.java
@@ -10,7 +10,31 @@ public class Charlie {
 		assert 1 <= m && m <= 100 : "out of range, m: " + m;
 		int s = scanner.nextInt();
 		assert 0 <= s && s <= 900 : "out of range, s: " + s;
-		
+		if (s > 9 * m || (s == 0 && m > 1)) {
+			System.out.println("-1 -1");
+			return;
+		}
+		if (m == 1 && s == 0) {
+			System.out.println("0 0");
+			return;
+		}
+		StringBuilder sb = new StringBuilder();
+		int l = 0;
+		for (int i = 0; i < m; i++) {
+			int d = (s >= 9) ? 9 : s;
+			sb.append(d);
+			s -= d;
+			if (d != 0) {
+				l = i;
+			}
+		}
+		String large = sb.toString();
+		if (sb.charAt(m - 1) == '0') {
+			sb.setCharAt(l, (char)(sb.charAt(l) - 1));
+			sb.setCharAt(m - 1, '1');
+		}
+		String small = sb.reverse().toString();
+		System.out.printf("%s %s", small, large);
 	}
 
 }
