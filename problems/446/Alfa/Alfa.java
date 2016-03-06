eb38272b62938168e906ea963b49296271976b4a
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2016-03-06 15:55:33 +0100

    446A fail again

--- problems/446/Alfa/Alfa.java
+++ problems/446/Alfa/Alfa.java
@@ -1,62 +1,56 @@
 import java.util.*;
 import java.io.*;
 
-/* Mighty Cohadar */
+/**
+  * @author Mighty Cohadar 
+  */
 public class Alfa {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		assert 1 <= n && n <= 1e5 : "out of range, n: " + n;
-		int[] A = scanArray(scanner, n);
-		int[] L = new int[n];
-		int[] F = new int[n];
-		int[] T = new int[n];
-		L[0] = 1;
-		F[0] = 1;
-		T[0] = 1;
-		for (int k = 1; k < n; k++) {
-			// L
-			int max = L[k-1];
-			if (A[k-1] < A[k]) {
-				max = Math.max(max, 1 + T[k-1]);
-			} else {
-				max = Math.max(max, 1 + F[k-1]);
-				max = Math.max(max, T[k-1]);
-			}
-			L[k] = max;
-			// F
-			max = 1;
-			if (A[k-1] < A[k]) {
-				max = Math.max(max, 1 + F[k-1]);
-			}
-			F[k] = max;
-			// T
-			max = F[k-1];
-			if (A[k-1] < A[k]) {
-				max = Math.max(max, 1 + T[k-1]);
-			} else {
-				if (k == n-1 || A[k+1] - A[k-1] >= 2) {
-					max = Math.max(max, 1 + F[k-1]);
-				}
-			}
-			T[k] = max;
-		}
-		debug('A', A);
-		debug('L', L);
-		debug('T', T);
-		debug('F', F);
-		System.out.println(L[n-1]);
+	final int n;
+	final int[] A; // 1-based
+	final int[] E;
+	final int[] R;
+	
+	public Alfa(int n, int[] A) {
+		this.n = n;
+		this.A = A;
+		this.E = new int[A.length];
+		this.R = new int[A.length];
 	}
 
-	static int[] scanArray(Scanner scanner, int n) {
-		int[] A = new int[n];
-		for (int i = 0; i < n; i++) {
+	public int getA(int j, int i) {
+		return (i == j) ? A[i - 1] + 1 : A[i];
+	}
+
+	public int solve() {
+		int max = 1;
+		E[1] = 1;
+		R[1] = 1;
+		for (int k = 2; k <= n; k++) {
+			E[k] = (condition) ? "YES" : "NO"
+		}
+		return max;
+	}
+
+	public static Alfa load(Scanner scanner) {
+		int n = scanner.nextInt();
+		assert 1 <= n && n <= 1e5 : "out of range, n: " + n;
+		scanner.nextLine();
+		int[] A = new int[1 + n];
+		for (int i = 1; i <= n; i++) {
 			A[i] = scanner.nextInt();
 			assert 1 <= A[i] && A[i] <= 1e9 : "out of range, A[i]: " + A[i];
 		}
-		return A;
+		return new Alfa(n, A);
+	} 
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Alfa o = Alfa.load(scanner);
+		System.out.println(o.solve());
 	}
+
+
 
 	static boolean DEBUG = true;
 	
@@ -64,6 +58,4 @@ public class Alfa {
 		if (!DEBUG) { return; }
 		System.err.printf("%.65536s\n", Arrays.deepToString(os));
 	}
-
 }
-
