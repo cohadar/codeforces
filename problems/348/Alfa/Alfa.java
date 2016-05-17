6d01a15b6f50a449364598a3bf5dfefda40ae276
cohadar <cohadar@gmail.com>
2016-05-17 17:49:12 +0200

    evil mafia

--- problems/348/Alfa/Alfa.java
+++ problems/348/Alfa/Alfa.java
@@ -4,33 +4,40 @@ import java.io.*;
 /* Mighty Cohadar */
 public class Alfa {
 
+	static final int INF = Integer.MAX_VALUE / 2;
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		assert 3 <= n && n <= 1e5 : "out of range, n: " + n;
+		NavigableSet<Integer> S = new TreeSet<>();
 		int[] A = new int[n];
+		long sum = 0;
 		for (int i = 0; i < n; i++) {
-			int a = scanner.nextInt();
-			assert 1 <= a && a <= 1e9 : "out of range, a: " + a;
-			A[i] = a;
+			A[i] = scanner.nextInt();
+			S.add(A[i]);
+			sum += A[i];
 		}
-		Arrays.sort(A);
-		int games = 0;
-		while (A[n - 1] > games) {
-			A[0]++;
-			games++;
-			for (int i = 0; i < n - 1; i++) {
-				if (A[i] > A[i + 1]) {
-					int temp = A[i];
-					A[i] = A[i + 1];
-					A[i + 1] = temp;
-				} else {
-					break;
-				}
-			}
+		long rounds = 0;
+		long sum2 = 0;
+		while (sum > sum2) {
+			int a = S.pollFirst();
+			int b = S.first();
+			int d = b - a + 1;
+			S.add(a + d);
+			sum2 += (n - 1L) * d;
+			rounds += d;
 		}
-		System.out.println(games);
+		int d = (int)(sum2 - sum);
+		int max = 0;
+		for (int s : S) {
+			max = Math.max(max, s - d);
+		}
+		System.out.println(rounds + max);
 	}
 
+	static void debug(Object...os) {
+		System.err.printf("%.65536s\n", Arrays.deepToString(os));
+	}
+	
 }
 
