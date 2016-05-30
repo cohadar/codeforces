fefcdaf34a36441c6402afc7e4c7bf5b9acfbd82
cohadar <cohadar@gmail.com>
2016-05-30 14:26:10 +0200

    meh

--- problems/158/Alfa/Alfa.py
+++ problems/158/Alfa/Alfa.py
@@ -1,14 +1,16 @@
 from __future__ import print_function
 import sys
 
+def debug(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def load(stdin):
-	n, k = map(int, stdin.readline().split(" "))
-	A = map(int, stdin.readline().split(" "))
+	n, k = map(int, stdin.readline().split())
+	A = map(int, stdin.readline().split())
 	border = A[k-1] if A[k-1] > 0 else 1
-	print(len(filter(lambda x: x >= border, A)));
+	print(len([x for x in A if x >= border]));
+	debug(A)
 
 if __name__ == '__main__':
 	load(sys.stdin)
 
-def debug(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
