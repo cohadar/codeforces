398c3619f8a8fefbca90ed586265e8c1a5473626
cohadar <cohadar@gmail.com>
2016-06-03 06:53:07 +0200

    667D cp

--- problems/667/Delta/Delta.java
+++ problems/667/Delta/Delta.java
@@ -6,6 +6,10 @@ import java.io.*;
   */
 public class Delta {
 
+	// E, S, W, N
+	static int[] DX = new int[] { +1, 00, -1, 00 };
+	static int[] DY = new int[] { 00, +1, 00, -1 };
+
 	final int ny;
 	final int nx;
 	final int pp;
@@ -20,22 +24,43 @@ public class Delta {
 		this.LL = LL;
 	}
 
-	public void solve(List<Cell> A, List<Cell> B) {
+	public int dist(Cell a, Cell b) {
+		return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+	}
+
+	public void solveDirect(List<Cell> A, List<Cell> B) {
 		for (Cell b : B) {
+			int min = D[b.y][b.x];
 			for (Cell a : A) {
-				b.d = Math.min(b.d, a.d + a.dist(b));
+				min = Math.min(min, D[a.y][a.x] + dist(a, b));
 			}
+			D[b.y][b.x] = min;
 		}
+	}
+
+	public boolean outside(int x, int y) {
+		return x < 0 || y < 0 || x >= nx || y >= ny;
+	}
+
+	public void solveBFS(List<Cell> A, List<Cell> B) {
+		solveDirect(A, B);
 	}
 
 	public int solve() {
 		for (int i = 1; i < pp; i++) {
-			solve(LL.get(i-1), LL.get(i));
+			List<Cell> A = LL.get(i-1);
+			List<Cell> B = LL.get(i);
+			if (A.size() * B.size() <= ny * nx) {
+				solveDirect(A, B);	
+			} else {
+				solveBFS(A, B);	
+			}
 		}
-		return LL.get(pp-1).get(0).d;
+		Cell c = LL.get(pp-1).get(0);
+		return D[c.y][c.x];
 	}
 
-	public static Delta load(Scanner scanner) {
+	public static Delta load(FastScanner scanner) {
 		int ny = scanner.nextInt();
 		int nx = scanner.nextInt();
 		int pp = scanner.nextInt();
@@ -44,11 +69,13 @@ public class Delta {
 			LL.add(new ArrayList<Cell>());
 		}
 		int[][] D = new int[ny][nx];
+		int[][] M = new int[ny][nx];
 		for (int y = 0; y < ny; y++) {
 			for (int x = 0; x < nx; x++) {
-				D[y][x] = scanner.nextInt() - 1;
-				Cell c = new Cell(x, y, (D[y][x] == 0) ? x + y : Integer.MAX_VALUE);
-				LL.get(D[y][x]).add(c);
+				M[y][x] = scanner.nextInt() - 1;
+				Cell c = new Cell(x, y);
+				LL.get(M[y][x]).add(c);
+				D[y][x] = (M[y][x] == 0) ? x + y : Integer.MAX_VALUE;
 			}
 		}
 
@@ -56,9 +83,13 @@ public class Delta {
 	} 
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		FastScanner scanner = new FastScanner(System.in);
 		Delta o = Delta.load(scanner);
 		System.out.println(o.solve());
+	}
+
+	static void debug(Object...os) {
+		System.err.printf("%.65536s\n", Arrays.deepToString(os));
 	}
 
 }
@@ -66,16 +97,52 @@ public class Delta {
 class Cell {
 	final int x;
 	final int y;
-	int d;
-	Cell(int x, int y, int d) {
+	Cell(int x, int y) {
 		this.x = x;
 		this.y = y;
-		this.d = d;
-	}
-	public int dist(Cell b) {
-		return Math.abs(this.x - b.x) + Math.abs(this.y - b.y);
 	}
 	public String toString() {
-		return String.format("(x=%d, y=%d, d=%d)", x, y, d);
+		return String.format("(x=%d, y=%d)", x, y);
 	}	
 }
+
+class FastScanner {
+	private final BufferedReader in;
+	private final StringBuilder sb;
+	private StringTokenizer strtok;
+	public FastScanner(InputStream is) {
+		this.in = new BufferedReader(new InputStreamReader(is));
+		this.sb = new StringBuilder();
+		this.strtok = null;
+	}
+	public String next() {
+		try {
+			if (strtok == null || strtok.hasMoreTokens() == false) {
+				strtok = new StringTokenizer(in.readLine());
+			}
+			return strtok.nextToken();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+	public long nextLong() {
+		return Long.parseLong(next());
+	}
+	public String nextLine() {
+		try {
+			if (strtok == null) {
+				return in.readLine();
+			} else {
+				String ret = (strtok.hasMoreTokens()) ? strtok.nextToken("\n") : "";
+				strtok = null;
+				return ret;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
+
