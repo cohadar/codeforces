08db18d01bc771d861ab4cac8db55cf6e466404f
cohadar <cohadar@gmail.com>
2016-06-22 12:09:46 +0200

    579A

---
+++ problems/579/Alfa/Alfa.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+/* Mighty Cohadar */
+public class Alfa {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		System.out.println(Integer.bitCount(x));
+	}
+	
+}
+
