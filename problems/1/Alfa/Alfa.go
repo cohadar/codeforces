9dfc27cdee220452539ff6be7004c98b18960f7d
cohadar <cohadar@gmail.com>
2018-06-30 09:18:27 +0200

    njaj

--- problems/1/Alfa/Alfa.go
+++ problems/1/Alfa/Alfa.go
@@ -3,11 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var n int64
-	var m int64
-	var a int64
-	fmt.Scanf("%d %d %d", &n, &m, &a)
-	x := (n + a - 1) / a
-	y := (m + a - 1) / a
-	fmt.Printf("%d\n", x * y)
+	var n, m, a int64
+	fmt.Scan(&n, &m, &a)
+	n1 := (n + a - 1) / a
+	m1 := (m + a - 1) / a
+	fmt.Println(n1 * m1)
 }
--- problems/50/Alfa/Alfa.go
+++ problems/50/Alfa/Alfa.go
@@ -1,17 +1,9 @@
-package main 
+package main
 
 import "fmt"
 
-func solve(a, b int) int {
-	if a % 2 == 0 || b % 2 == 0 {
-		return a * b / 2
-	} else {
-		return (a * b - 1) / 2
-	}
-}
-
 func main() {
-	var a, b int
-	fmt.Scanln(&a, &b)
-	fmt.Println(solve(a, b))
+	var m, n int
+	fmt.Scan(&m, &n)
+	fmt.Println((m * n) / 2)
 }
