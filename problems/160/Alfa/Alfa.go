65c524b3c7cbbc4a6d42c77bdcff608ba5e3bbb7
cohadar <cohadar@gmail.com>
2018-06-30 10:37:08 +0200

    meh

--- problems/160/Alfa/Alfa.go
+++ problems/160/Alfa/Alfa.go
@@ -5,23 +5,20 @@ import "sort"
 
 func main() {
 	var n int
-	fmt.Scanln(&n)
+	fmt.Scan(&n)
 	a := make([]int, n)
 	sum := 0
 	for i := 0; i < n; i++ {
 		fmt.Scan(&a[i])
 		sum += a[i]
 	}
-	sort.Ints(a)
-	l, r := 0, sum
-	nr := len(a)
-	for _, v := range a {
-		if l + v < r - v {
-			l += v
-			r -= v
-			nr--
+	sort.Sort(sort.Reverse(sort.IntSlice(a)))
+	curr := 0
+	for i := 0; i < n; i++ {
+		curr += a[i]
+		if 2*curr > sum {
+			fmt.Println(i + 1)
+			return
 		}
 	}
-	fmt.Println(nr)
 }
-
