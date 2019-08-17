60d13e346e9f06393ebbd346fc678130ca9799e4
cohadar <cohadar@gmail.com>
2019-08-17 16:21:17 +0200

    meh

--- problems/1202/Alfa/Alfa.py
+++ problems/1202/Alfa/Alfa.py
@@ -1,17 +1,5 @@
-def dz(y):
-    c = 0
-    for x in reversed(y):
-        if x == '0':
-            c += 1
-        else:
-            break;
-    return c
-
-def score(x, y):
-    d = dz(y)
-    if d == 0:
-        return dz(x)
-    return dz(x[:-d])
-
 for _ in range(int(input())):
-    print(score(input(), input()))
+    x, y = input()[::-1], input()[::-1]
+    iy = y.index('1')
+    ix = x.index('1', iy)
+    print(ix - iy)
