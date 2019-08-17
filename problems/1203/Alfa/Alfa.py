172705f57371ed55cd97f4280a6373fb9ad1301b
cohadar <cohadar@gmail.com>
2019-08-17 14:44:40 +0200

    meh

--- problems/1203/Alfa/Alfa.py
+++ problems/1203/Alfa/Alfa.py
@@ -1,22 +1,6 @@
-import sys
-
-def left(l, r, n):
-    return (l + 1) % n == r
-
-def right(l, r, n):
-    return (r + 1) % n == l
-
-def main():
-    lines = (line for line in sys.stdin)
-    for q in range(int(next(lines))):
-        n = int(next(lines))
-        a = [int(x) - 1 for x in next(lines).split()]
-        z1 = zip(a[:-1], a[1:])
-        z2 = zip(a[:-1], a[1:])
-        if all(left(*x, n) for x in z1) or all(right(*x, n) for x in z2):
-            print('YES')
-        else:
-            print('NO')
-
-if __name__ == '__main__':
-    main()
+for q in range(int(input())):
+    n = int(input())
+    a = [int(x) - 1 for x in input().split()]
+    b1 = all((l + 1) % n == r for (l, r) in zip(a[:-1], a[1:]))
+    b2 = all((l + 1) % n == r for (r, l) in zip(a[:-1], a[1:]))
+    print('YES') if b1 or b2 else print('NO')
